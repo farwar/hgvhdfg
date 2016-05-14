1ca9281beab415a8834d585d644a4b659702e467
farwar <azagrizliy@mail.ru>
2016-05-14 20:06:52 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,70 @@
-bx=300 
-by=300 
-bxs=0 
-bys=0 
-r=580 
-n=280 
-def setup(): 
-size(600,600) 
-def draw(): 
-global bxs,bys,bx,by,r,n 
-rect(10,10,580,590) 
-rect(n,r,70,10) 
-ellipse(bx,by,10,10) 
-bx-=bxs 
-by+=bys 
-if by>=574 and bx>=n and bx<=n+70: 
-bys=-bys 
-if keyPressed: 
-if key == 'p': 
-bxs=+3 
-bys=-3 
-if key == 'w' and n>=15: 
-n-=5 
-if key == 's' and n<=515: 
-n+=5 
-
-if bx<=580: 
-bxs=-bxs 
-if by>=16: 
-bys=-bys 
-if by<=590: 
-bys=-bys 
-if bx>=16: 
-bxs=-bxs
+bx=300
+by=300
+bxs=0
+bys=0
+ry=250
+rx=250
+rxs=3
+rys=3
+x=250
+def setup():
+    global bx,bx
+    size(600,600)
+def draw():
+    global bx,by,bys,bxs,ry,rx,rxs,rys,x
+    fill(0,255,0)
+    rect(5,100,595,400)
+    line(300,100,300,500)
+    line(5,300,595,300)
+    triangle(275,100,325,100,300,125)
+    triangle(275,500,325,500,300,475)
+    fill(0,0,255)
+    rect(580,rx,10,100)
+    if keyPressed:
+        if key == 'o' and rx>=104:
+            rx-=5
+        if key == 'l' and rx<=396:
+            rx+=5
+    fill(255,0,0)
+    rect(20,ry,10,100)
+    if keyPressed:
+        if key == 'w' and ry>=104:
+            ry-=5
+        if key == 's' and ry<=396:
+            ry+=5
+    fill(255,254,0)
+    ellipse(bx,by,15,15)
+    if keyPressed:
+          if key == 'p':
+              bxs=-3
+              bys=3
+          if key == 'c':
+              bxs=0
+              bys=0
+    bx+=bxs
+    by+=bys
+    if bx<=18:
+        bxs=-bxs
+    if by>=485:
+        bys=-bys
+    if by<=120:
+        bys=-bys
+    if bx>=600:
+        bxs=-bxs
+    if bx<=40 and by>=ry and by<=ry+100:
+        bxs=-bxs
+    if bx>=574 and by>=rx and by<=rx+100:
+        bxs=-bxs
+        bys=-bys
+    if bx<=5:
+        bxs=0
+        bys=0
+        bx=width/2
+        by=height/2
+    if bx>=595:
+        bxs=0
+        bys=0
+        bx=width/2
+        by=height/2
+        
 
